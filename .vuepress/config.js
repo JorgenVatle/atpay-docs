5a85fae6fc6e143c86c45d5d90aa5a641bc48490
Jørgen Vatle <Jorgen@WcAServices.net>
2018-09-29 21:27:17 +0300

    Add navigation links to VuePress config.

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,20 @@
 module.exports = {
     title: 'ATPay',
-    description: 'Developer reference for ATPay.'
+    description: 'Developer reference for ATPay.',
+    themeConfig: {
+        nav: [
+            {
+                text: 'Introduction',
+                link: '/'
+            },
+            {
+                text: 'Reference',
+                link: '/reference',
+            },
+            {
+                text: 'Admin Dashboard',
+                link: 'https://atpay.io'
+            }
+        ]
+    }
 };
